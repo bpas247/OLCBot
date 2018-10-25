d09c96262d1ee247f8f4c5840f133e950b71f5f8
Brady Pascoe <bpas247@hotmail.com>
2018-10-25 13:07:51 -0700

    add testing and CI

--- cogs/cog.js
+++ cogs/cog.js
@@ -1,4 +1,7 @@
 const birthday = require('./birthday.js').birthday;
+const complain = require('./messages.js').complain;
+const sass = require('./messages.js').sassy;
+const motivate = require('./messages.js').motivate;
 
 const cogs = new Map
   ([
@@ -24,41 +27,44 @@ const cogs = new Map
     [
       "complain",
       () => {
-        return randomGrab([
-          "Why doesn't anyone use introductory sentences?",
-          "I'm not a style guide!",
-          "Just google it!!",
-          "TOO MUCH PERSONAL INFORMATION—MY EYES ARE BLEEDING",
-          "Why is it so cold in here?",
-          "Whyyy is it so hot in here???",
-          "Did you remember to sign in?",
-          "I rephrased this question 27 times and drew 4 visuals.",
-          "Why is there a banana here?",
-          "Don't forget the '+ c'."
-        ])
-      }
+      //   return randomGrab([
+      //     "Why doesn't anyone use introductory sentences?",
+      //     "I'm not a style guide!",
+      //     "Just google it!!",
+      //     "TOO MUCH PERSONAL INFORMATION—MY EYES ARE BLEEDING",
+      //     "Why is it so cold in here?",
+      //     "Whyyy is it so hot in here???",
+      //     "Did you remember to sign in?",
+      //     "I rephrased this question 27 times and drew 4 visuals.",
+      //     "Why is there a banana here?",
+      //     "Don't forget the '+ c'."
+      //   ])
+      return randomGrab(complain);
+       }
     ],
     [
       "do",
       () => {
-        return randomGrab([
-          "no.",
-          "Let me get back to you on that :information_desk_person:",
-          "Here's a suggestion: how about you do it?",
-          "Could you ask for nothing? I'll be able to do nothing."
-        ])
+        // return randomGrab([
+        //   "no.",
+        //   "Let me get back to you on that :information_desk_person:",
+        //   "Here's a suggestion: how about you do it?",
+        //   "Could you ask for nothing? I'll be able to do nothing."
+        // ])
+        return randomGrab(sass);
       }
     ],
     [
       "motivate",
       () => {
-        return randomGrab([
-          "students who visit the Bock Learning Center have improved grades",
-          "a student has successfully understood the concept you explained",
-          "a student has signed in successfully",
-          "a student has pushed in their chair as they left",
-          "there are no students left in the center at closing time"
-        ])
+        // return randomGrab([
+        //   "students who visit the Bock Learning Center have improved grades",
+        //   "a student has successfully understood the concept you explained",
+        //   "a student has signed in successfully",
+        //   "a student has pushed in their chair as they left",
+        //   "there are no students left in the center at closing time"
+        // ])
+        return randomGrab(motivate);
       }
     ],
     [
---
+++ cogs/messages.js
@@ -0,0 +1,33 @@
+const motivate = [
+  "students who visit the Bock Learning Center have improved grades",
+  "a student has successfully understood the concept you explained",
+  "a student has signed in successfully",
+  "a student has pushed in their chair as they left",
+  "there are no students left in the center at closing time"
+];
+
+const sassy = [
+    "no.",
+    "Let me get back to you on that :information_desk_person:",
+    "Here's a suggestion: how about you do it?",
+    "Could you ask for nothing? I'll be able to do nothing."
+];
+
+const complain = [
+    "Why doesn't anyone use introductory sentences?",
+    "I'm not a style guide!",
+    "Just google it!!",
+    "TOO MUCH PERSONAL INFORMATION—MY EYES ARE BLEEDING",
+    "Why is it so cold in here?",
+    "Whyyy is it so hot in here???",
+    "Did you remember to sign in?",
+    "I rephrased this question 27 times and drew 4 visuals.",
+    "Why is there a banana here?",
+    "Don't forget the '+ c'."
+];
+
+module.exports = {
+    motivate,
+    sassy,
+    complain
+};
