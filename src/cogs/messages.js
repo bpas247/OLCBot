1cb15f9dd3f4e8c200d1cade343637400ad2ccb3
Brady Pascoe <bpas247@hotmail.com>
2018-10-31 20:47:49 -0700

    fixed formatting

--- src/cogs/messages.js
+++ src/cogs/messages.js
@@ -34,7 +34,7 @@ const help =
   '!motivate - Generate a random motivation\n' +
   '!alive - How long have I been alive for?\n' +
   '!birthday add MM/DD/YYYY - add a birthday to the list\n' +
-  "!birthday ls - list everyone's birthday" +
+  "!birthday ls - list everyone's birthday\n" +
   '!trickOrTreat - _spooky_ game of heads or tails';
 
 module.exports = {
