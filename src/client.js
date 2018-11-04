835ba97c9f03f084bc27cf521c78074526e8a6f3
Brady Pascoe <bpas247@hotmail.com>
2018-11-04 15:13:43 -0800

    added creation tests

--- src/client.js
+++ src/client.js
@@ -10,6 +10,17 @@ import { onCreate, onMessage } from './bot';
 client.on(
   'ready',
   async () => {
+    // This event will run if the bot starts, and logs in, successfully.
+    console.log(
+      `Bot has started, with ${client.users.size} users, in ${
+        client.channels.size
+      } channels of ${client.guilds.size} guilds.`
+    );
+
+    // Example of changing the bot's playing game to something useful. `client.user` is what the
+    // docs refer to as the "ClientUser".
+    client.user.setActivity(`Type !help for more info`);
+
     await onCreate(client);
   },
   err => {
