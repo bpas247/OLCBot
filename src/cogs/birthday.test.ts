e435c311e2f5fcfb1ac5e00fc280e9535b86c941
Brady Pascoe <18705892+bpas247@users.noreply.github.com>
2019-02-26 02:59:42 -0800

    reduce redundant code in tests

--- src/cogs/birthday.test.ts
+++ src/cogs/birthday.test.ts
@@ -81,40 +81,37 @@ describe("listUsers", () => {
 });
 
 describe("updateEntry", () => {
-  it("Should add a new entry", async () => {
-    let testId: number = 2124588682;
-    let testDate: string = "December 5, 1981";
-    let testArray: Array<Object> = [];
-    let db: any = {
+  let testId: number;
+  let testDate: string;
+  let testArray: Array<Object>;
+  let db: any;
+
+  beforeEach(() => {
+    testId = 2124588682;
+    testDate = "December 5, 1981";
+    testArray = [];
+    db = {
       query: sinon.spy()
     }
-
+  });
+  
+  it("Should add a new entry", async () => {
     let returns = await updateEntry(testId, testDate, testArray, db);
     expect(returns).toEqual('Added new entry!');
     expect(db.query.calledOnce).toBeTruthy();
   });
   
   it("Should update an existing entry", async () => {
-    let testId: number = 2124588682;
-    let testDate: string = "December 5, 1981";
-    let testArray: Array<Object> = [new Object({ id: testId, date: "December 7, 1981" })];
-    let db: any = {
-      query: sinon.spy()
-    }
-
+    testArray = [new Object({ id: testId, date: "December 7, 1981" })];
+    
     let returns = await updateEntry(testId, testDate, testArray, db);
     expect(returns).toEqual('Updated entry!');
     expect(db.query.calledOnce).toBeTruthy();
   });
 
   it("Should not update the db if nothing changed", async () => {
-    let testId: number = 2124588682;
-    let testDate: string = "December 5, 1981";
-    let testArray: Array<Object> = [new Object({ id: testId, date: testDate })];
-    let db: any = {
-      query: sinon.spy()
-    }
-
+    testArray = [new Object({ id: testId, date: testDate })];
+    
     let returns = await updateEntry(testId, testDate, testArray, db);
     expect(returns).toEqual("Name and date already in the database, so I'm not gonna re-add it.");
     expect(db.query.callCount).toBe(0);
