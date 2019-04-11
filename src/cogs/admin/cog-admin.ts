1050243c8abc9e69d6fc768c458d929904298e07
Brady Pascoe <18705892+bpas247@users.noreply.github.com>
2019-04-10 20:31:43 -0700

    separate out admin logic to its own cog

---
+++ src/cogs/admin/cog-admin.ts
@@ -0,0 +1,34 @@
+import Cog from '../cog';
+import { GuildMember, Message } from "discord.js";
+import { IDatabase } from "pg-promise";
+
+class CogAdmin extends Cog {
+    public constructor(
+        _command: string,
+        _func: (
+            message: Message,
+            args: Array<string>,
+            db: IDatabase<any>
+        ) => Promise<string | undefined> | string,
+        _help?: string,
+        _args?: Array<Cog>
+    ) { super(_command, _func, _help, _args) }
+    
+    public async run (message: Message, args: Array<string>, db: IDatabase<any>) {
+        let author: GuildMember | undefined = undefined;
+        if (message.guild)
+            author = await message.guild.fetchMember(message.author);
+        else return "Command does not work in DM";
+
+        if (!author) return "Could not find the user in the server to authenticate";
+
+        let isAdmin = author.hasPermission("ADMINISTRATOR");
+
+        if (!isAdmin)
+            return "You don't have the permissions for this command.";
+
+        return super.run(message, args, db);
+    }
+}
+
+export default CogAdmin;
