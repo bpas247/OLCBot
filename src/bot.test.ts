e787d8e52b9a3317cc773af1267a10ac75d71b11
Brady Pascoe <18705892+bpas247@users.noreply.github.com>
2019-08-09 21:16:49 -0700

    ci: actual fix for tests not resolving

--- src/bot.test.ts
+++ src/bot.test.ts
@@ -1,8 +1,9 @@
 import { onCreate, onCommand } from './bot';
 import sinon from 'sinon';
-import pgPromise from 'pg-promise';
+import pgPromise, { IDatabase, IMain } from 'pg-promise';
 
-const db = pgPromise()({
+const pgp = pgPromise();
+const db = pgp({
   host: 'localhost',
   port: 5432,
   database: 'test',
@@ -10,10 +11,14 @@ const db = pgPromise()({
   password: (process.env.DATABASE_PASSWORD ? process.env.DATABASE_PASSWORD : undefined)
 });
 
+
 describe('bot', () => {
   beforeEach(() => {
     db.any("DROP TABLE IF EXISTS birthday");
     db.any("DROP TABLE IF EXISTS memes");
+  });
+  afterAll(() => {
+    pgp.end();
   })
   describe("onCreate", () => {
     it("should not throw an error on create", async () => {
