8704e177b600ce831b601d2167539e5e4b11d2da
Brady Pascoe <18705892+bpas247@users.noreply.github.com>
2019-08-09 20:54:50 -0700

    test: migrate testing to use an actual database
    Migrates the testing to use an actual database, rather than
    mocking everything.

--- src/bot.test.ts
+++ src/bot.test.ts
@@ -1,43 +1,53 @@
 import { onCreate, onCommand } from './bot';
 import sinon from 'sinon';
+import pgPromise from 'pg-promise';
 
-const dbMock: any = {
-  query: sinon.spy()
-}
-
-describe("onCreate", () => {
-  it("should create the databases", async () => {
-    await onCreate(dbMock);
-    expect(dbMock.query.callCount).toEqual(2);  
-  });
+const db = pgPromise()({
+  host: 'localhost',
+  port: 5432,
+  database: 'test',
+  user: 'postgres',
+  password: (process.env.DATABASE_PASSWORD ? process.env.DATABASE_PASSWORD : undefined)
 });
 
-describe("onCommand", () => {
-  let messageMock: any;
-
+describe('bot', () => {
   beforeEach(() => {
-    messageMock = {
-      content: "!ping",
-      channel: {
-        send: sinon.spy()
+    db.any("DROP TABLE IF EXISTS birthday");
+    db.any("DROP TABLE IF EXISTS memes");
+  })
+  describe("onCreate", () => {
+    it("should not throw an error on create", async () => {
+      await onCreate(db);
+    });
+  });
+
+  describe("onCommand", () => {
+    let messageMock: any;
+
+    beforeEach(() => {
+      messageMock = {
+        content: "!ping",
+        channel: {
+          send: sinon.spy()
+        }
       }
-    }    
-  });
+    });
 
-  it("should make sure it can resolve a simple command", async () => {
-    await onCommand(messageMock, dbMock);
-    expect(messageMock.channel.send.calledWith('Pong!')).toBeTruthy();  
-  });
+    it("should make sure it can resolve a simple command", async () => {
+      await onCommand(messageMock, db);
+      expect(messageMock.channel.send.calledWith('Pong!')).toBeTruthy();
+    });
 
-  it("should not send a message to a channel if the prefix is invalid", async () => {
-    messageMock.content = ">poing";
-    await onCommand(messageMock, dbMock);
-    expect(messageMock.channel.send.callCount).toEqual(0);  
-  });
+    it("should not send a message to a channel if the prefix is invalid", async () => {
+      messageMock.content = ">poing";
+      await onCommand(messageMock, db);
+      expect(messageMock.channel.send.callCount).toEqual(0);
+    });
 
-  it("should reply with an unrecognized command if passed in an invalid command", async () => {
-    messageMock.content = "!poing";
-    await onCommand(messageMock, dbMock);
-    expect(messageMock.channel.send.calledWith('Command not recognized: Base Cog undefined')).toBeTruthy();  
+    it("should reply with an unrecognized command if passed in an invalid command", async () => {
+      messageMock.content = "!poing";
+      await onCommand(messageMock, db);
+      expect(messageMock.channel.send.calledWith('Command not recognized: Base Cog undefined')).toBeTruthy();
+    });
   });
-});
+})
